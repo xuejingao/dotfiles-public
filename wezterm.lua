1bbf681bbf34bf3c85a0d7de0aeddf1ee7ee0829
Xuejin <xuejingao@gmail.com>
2026-06-27 01:25:18 -0400

    update files

--- wezterm.lua
+++ wezterm.lua
@@ -1,18 +1,33 @@
--- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices.
+local is_windows = os.getenv("OS") and os.getenv("OS"):lower():find("windows")
+local is_macos = wezterm.target_triple:lower():find("darwin") ~= nil
 
--- For example, changing the initial geometry for new windows:
-config.initial_cols = 120
-config.initial_rows = 28
+config.color_scheme = "rose-pine-moon"
+config.max_fps = 120
+config.font = wezterm.font("Hack Nerd Font", { weight = "DemiBold" })
+config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+config.window_frame = {
+  font = wezterm.font("Hack Nerd Font", { weight = "Bold" }),
+}
+config.inactive_pane_hsb = {
+  saturation = 0.0,
+  brightness = 0.5,
+}
 
--- or, changing the font size and color scheme.
-config.font_size = 10
-config.color_scheme = 'AdventureTime'
+if is_windows then
+  config.win32_system_backdrop = "Acrylic"
+  config.window_background_opacity = 0.7
+  config.window_frame.font_size = 10.0
+end
 
--- Finally, return the configuration to wezterm:
+if is_macos then
+  config.window_background_opacity = 0.8
+  config.macos_window_background_blur = 50
+  config.font_size = 15.0
+  config.window_frame.font_size = 13.0
+end
+
 return config
